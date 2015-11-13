a4768dcb6d00bcae494990d63b3a5fd8feaa406b
Enrico Hoffmann <dasrick@gmail.com>
2015-11-13 14:42:24 +0100

    initial stuff ... its running

---
+++ main.js
@@ -0,0 +1,20 @@
+var app = require('app');  // Module to control application life.
+var BrowserWindow = require('browser-window');  // Module to create native browser window.
+
+// Report crashes to our server.
+require('crash-reporter').start();
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+var mainWindow = null;
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+app.on('ready', function() {
+  mainWindow = new BrowserWindow({width: 800, height: 600});
+  mainWindow.loadUrl('file://' + __dirname + '/index.html');
+  //mainWindow.openDevTools();
+  mainWindow.on('closed', function() {
+    mainWindow = null;
+  });
+});
